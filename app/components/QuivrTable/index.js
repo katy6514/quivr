258d57321d2a810629916a442138f0429d7ae900
Katherine Wyman <katy6514@gmail.com>
2020-04-10 15:53:02 -0700

    Working sortable table!

--- app/components/QuivrTable/index.js
+++ app/components/QuivrTable/index.js
@@ -9,57 +9,282 @@ import React from 'react';
 // import styled from 'styled-components';
 
 import {
+  Checkbox,
+  IconButton,
+  Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  TableSortLabel,
+  Toolbar,
+  Tooltip,
+  Typography,
 } from '@material-ui/core';
 
-import { needles } from './utils';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { rows, headCells } from './utils';
 
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
-function QuivrTable() {
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function EnhancedTableHead(props) {
+  const {
+    classes,
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
   return (
-    <TableContainer>
-      <Table aria-label="simple table" size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Needle </TableCell>
-            <TableCell align="right">Size</TableCell>
-            <TableCell align="right">Length</TableCell>
-            <TableCell align="right">Material</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {needles.map(needle => (
-            <TableRow
-              key={
-                Math.random()
-                  .toString(36)
-                  .substring(2, 15) +
-                Math.random()
-                  .toString(36)
-                  .substring(2, 15)
-              }
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{ 'aria-label': 'select all desserts' }}
+          />
+        </TableCell>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
             >
-              <TableCell component="th" scope="needle">
-                {needle.type}
-              </TableCell>
-              <TableCell align="right">{needle.size}</TableCell>
-              <TableCell align="right">{needle.length}</TableCell>
-              <TableCell align="right">{needle.material}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span
+                  style={{
+                    border: 0,
+                    clip: 'rect(0 0 0 0)',
+                    height: 1,
+                    margin: -1,
+                    overflow: 'hidden',
+                    padding: 0,
+                    position: 'absolute',
+                    top: 20,
+                    width: 1,
+                  }}
+                >
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+const EnhancedTableToolbar = props => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar>
+      {numSelected > 0 ? (
+        <Typography color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography variant="h6" id="tableTitle" component="div">
+          Needle
+        </Typography>
+      )}
+
+      {numSelected > 0 && (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+function QuivrTable() {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('type');
+  const [selected, setSelected] = React.useState([]);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = event => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map(n => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const isSelected = name => selected.indexOf(name) !== -1;
+
+  console.log('rows', rows);
+
+  return (
+    <Paper>
+      <EnhancedTableToolbar numSelected={selected.length} />
+      <TableContainer>
+        <Table
+          aria-labelledby="tableTitle"
+          size="small"
+          aria-label="enhanced table"
+        >
+          <EnhancedTableHead
+            numSelected={selected.length}
+            order={order}
+            orderBy={orderBy}
+            onSelectAllClick={handleSelectAllClick}
+            onRequestSort={handleRequestSort}
+            rowCount={rows.length}
+          />
+          <TableBody>
+            {stableSort(rows, getComparator(order, orderBy)).map(
+              (row, index) => {
+                const isItemSelected = isSelected(row.name);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    onClick={event => handleClick(event, row.name)}
+                    role="checkbox"
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={
+                      Math.random()
+                        .toString(36)
+                        .substring(2, 15) +
+                      Math.random()
+                        .toString(36)
+                        .substring(2, 15)
+                    }
+                    selected={isItemSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        checked={isItemSelected}
+                        inputProps={{ 'aria-labelledby': labelId }}
+                      />
+                    </TableCell>
+                    <TableCell align="right">{row.type}</TableCell>
+                    <TableCell align="right">{row.size}</TableCell>
+                    <TableCell align="right">{row.length}</TableCell>
+                    <TableCell align="right">{row.material}</TableCell>
+                  </TableRow>
+                );
+              },
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
 
 QuivrTable.propTypes = {};
 
 export default QuivrTable;
+
+// <TableContainer>
+//   <Table aria-label="simple table" size="small">
+//     <TableHead>
+//       <TableRow>
+//         <TableCell>Needle </TableCell>
+//         <TableCell align="right">Size</TableCell>
+//         <TableCell align="right">Length</TableCell>
+//         <TableCell align="right">Material</TableCell>
+//       </TableRow>
+//     </TableHead>
+//     <TableBody>
+//       {rows.map(needle => (
+//         <TableRow
+//           key={
+//             Math.random()
+//               .toString(36)
+//               .substring(2, 15) +
+//             Math.random()
+//               .toString(36)
+//               .substring(2, 15)
+//           }
+//         >
+//           <TableCell component="th" scope="row">
+//             {needle.type}
+//           </TableCell>
+//           <TableCell align="right">{needle.size}</TableCell>
+//           <TableCell align="right">{needle.length}</TableCell>
+//           <TableCell align="right">{needle.material}</TableCell>
+//         </TableRow>
+//       ))}
+//     </TableBody>
+//   </Table>
+// </TableContainer>
--- app/components/QuivrTable/utils.js
+++ app/components/QuivrTable/utils.js
@@ -1,9 +1,16 @@
+export const headCells = [
+  { id: 'type', numeric: false, disablePadding: false, label: 'Type' },
+  { id: 'size', numeric: false, disablePadding: false, label: 'Size' },
+  { id: 'length', numeric: true, disablePadding: false, label: 'Length' },
+  { id: 'material', numeric: false, disablePadding: false, label: 'Material' },
+];
+
 function createData(type, size, length, material) {
   return { type, size, length, material };
 }
 
-export const needles = [
-  createData('Circular', 'US10', 6.0, 'metal'),
+export const rows = [
+  createData('Circularish', 'US10', 6.0, 'metal'),
   createData('Circular', 'US9', 9.0, 'bamboo'),
   createData('DBL Pointed', 'US12', 16.0, 'bamboo'),
 ];
