8a2738c708fef0f6e3d96128621a5e02b34af8f1
Katherine Wyman <katy6514@gmail.com>
2020-04-10 16:00:35 -0700

    some reorg of functions into util file

--- app/components/QuivrTable/index.js
+++ app/components/QuivrTable/index.js
@@ -26,36 +26,10 @@ import {
 
 import DeleteIcon from '@material-ui/icons/Delete';
 
-import { rows, headCells } from './utils';
+import { rows, headCells, getComparator, stableSort } from './utils';
 
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-}
 
 function EnhancedTableHead(props) {
   const {
@@ -187,8 +161,6 @@ function QuivrTable() {
   };
 
   const isSelected = name => selected.indexOf(name) !== -1;
-
-  console.log('rows', rows);
 
   return (
     <Paper>
